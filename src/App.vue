e3bbf70987becb67f2bffe53f5690bca82fc90d2
ligecarryme <965108861@qq.com>
2020-09-26 18:24:34 +0800

    optimization

--- src/App.vue
+++ src/App.vue
@@ -11,25 +11,20 @@
         </q-toolbar-title>
         <q-btn dense flat round icon="menu" @click="right = !right" />
       </q-toolbar>
-
-      <q-tabs v-model="tab" inline-label align="left" class="m-padded-left">
-        <q-route-tab to="/" name="home" icon="home" label="首页" />
-        <q-route-tab to="/catalog" name="catalog" icon="table_chart" label="分类" />
-        <q-route-tab to="/marks" name="marks" icon="bookmarks" label="标签" />
-        <q-route-tab to="/library" name="library" icon="library_books" label="归档" />
-        <q-route-tab to="/aboutme" name="aboutme" icon="perm_identity" label="关于我" />
-
-        <q-input
-          color="teal"
-          v-model="text"
-          label="search..."
-          class="m-padded-ab-right m-padded-bottom-tiny"
-        >
+      <div class="headerTabs">
+        <q-tabs v-model="tab" inline-label align="left" class="m-padded-left">
+          <q-route-tab to="/" name="home" icon="home" label="首页" />
+          <q-route-tab to="/catalog" name="catalog" icon="table_chart" label="分类" />
+          <q-route-tab to="/marks" name="marks" icon="bookmarks" label="标签" />
+          <q-route-tab to="/library" name="library" icon="library_books" label="归档" />
+          <q-route-tab to="/aboutme" name="aboutme" icon="perm_identity" label="关于我" />
+        </q-tabs>
+        <q-input color="teal" borderless v-model="text" label="search..." class="q-ml-md searchBar m-margin-right" align="right">
           <template v-slot:append>
-            <q-icon name="search" />
+            <q-icon name="search" class="cursor-pointer" />
           </template>
         </q-input>
-      </q-tabs>
+      </div>
     </q-header>
     <!-- 右侧边栏 -->
     <q-drawer v-model="right" side="right" overlay behavior="desktop" elevated>
@@ -39,8 +34,20 @@
     <q-page-container>
       <router-view />
     </q-page-container>
-    <!-- 底部footer -->
-    <!-- <q-footer elevated class="bg-grey-8 text-white">
+    <!-- 底部 -->
+    <div class="blogFooter">
+      <div class="footerfirst">
+        <h5>Blog</h5>
+        <p>这是我的个人博客、会分享关于编程、写作、思考相关的任何内容，希望可以给来到这儿的人有所帮助...</p>
+      </div>
+      <q-separator dark />
+      <div class="footersecond">
+        <p>网站已运行1天，托管于<a href="https://github.com/">Github</a></p>
+        <p>design by Darin</p>
+        <p>Copyright © 2020-2020</p>
+      </div>
+    </div>
+    <!-- <q-footer reveal elevated class="bg-grey-8 text-white">
       <q-toolbar>
         <q-toolbar-title>
           <q-avatar>
@@ -53,17 +60,54 @@
 </template>
 
 <script>
-
 export default {
   name: "App",
   components: {},
   data() {
     return {};
   },
-  created() { },
+  created() {},
 };
 </script>
 
-<style lang="sass" scoped>
-@import "./assets/css/me.css"
+<style lang="scss" scoped>
+@import "./assets/css/me.css";
+.headerTabs {
+  display: flex;
+  justify-content: space-between;
+}
+.searchBar {
+  overflow: hidden;
+}
+
+.blogFooter {
+  height: 150px;
+  color: #616161;
+  background-color: #232323;
+  text-align: center;
+  .footerfirst {
+    padding-top: 10px;
+    h5 {
+      color: aliceblue;
+      font-size: 20px;
+      margin: 0;
+    }
+    p {
+      margin: 5px 0px;
+    }
+  }
+  .footersecond {
+    padding-top: 5px;
+    a {
+      color: #795548;
+      z-index: 3;
+    }
+    a:hover {
+      color: aliceblue;
+    }
+  }
+  p {
+    margin: 0;
+  }
+}
 </style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,177 +1,181 @@
 <template>
   <div class="q-pa-md row">
-    <!-- 主页 -->
+    <!-- blog主页 -->
     <div class="col-lg-8 col-md-6 col-sm-12">
       <div class="row justify-center q-gutter-sm">
-        <q-intersection v-for="index in 6" :key="index" transition="scale" class="example-item">
-          <q-item to="/article" clickable v-ripple class="m-padded-zero">
-            <q-card class="q-ma-sm">
-              <img src="https://cdn.quasar.dev/img/mountains.jpg" />
+        <q-intersection v-for="index in 6" :key="index" transition="scale" class="card-item">
+          <q-card class="q-ma-sm">
+            <q-item clickable v-ripple class="imgItem">
+              <q-item-section>
+                <q-img src="https://cdn.quasar.dev/img/mountains.jpg" />
+              </q-item-section>
+            </q-item>
+            <q-card-section>
+              <div class="text-h6 blogTitle">
+                <a href="#">Card #{{ index }}</a>
+              </div>
+              <div class="text-subtitle2">by John Doe</div>
+            </q-card-section>
 
-              <q-card-section>
-                <div class="text-h6">Card #{{ index }}</div>
-                <div class="text-subtitle2">by John Doe</div>
-              </q-card-section>
-
-              <q-card-section class="q-pt-none">{{ content }}</q-card-section>
-            </q-card>
-          </q-item>
+            <q-card-section class="q-pt-none">{{ content }}</q-card-section>
+          </q-card>
         </q-intersection>
       </div>
       <div class="q-pa-lg flex flex-center">
         <q-pagination v-model="current" :max="10" :max-pages="6" :direction-links="true"></q-pagination>
       </div>
     </div>
-    
+
     <!-- 右边侧栏 -->
     <div class="col-lg-4 col-md-6 col-sm-12" align="center">
-      <div>
-        <q-card class="my-card m-margin-top-small">
-          <q-card-section class="bg-blue-3 text-white">
-            <div class="text-h6">分类</div>
-          </q-card-section>
-          <div class="q-pa-md" style="max-width: 100%">
-            <q-list bordered>
-              <q-item clickable v-ripple>
-                <q-item-section>Icon as avatar</q-item-section>
-                <q-item-section avatar>
-                  <q-icon color="primary" name="bluetooth" />
-                </q-item-section>
-              </q-item>
-              <q-separator />
+      <q-card class="my-card m-margin-top-small">
+        <q-card-section class="bg-blue-3 text-white">
+          <div class="text-h6">分类</div>
+        </q-card-section>
+        <div class="q-pa-md" style="max-width: 100%">
+          <q-list bordered>
+            <q-item clickable v-ripple>
+              <q-item-section>Icon as avatar</q-item-section>
+              <q-item-section avatar>
+                <q-icon color="primary" name="bluetooth" />
+              </q-item-section>
+            </q-item>
+            <q-separator />
 
-              <q-item clickable v-ripple>
-                <q-item-section>Avatar-type icon</q-item-section>
-                <q-item-section avatar>
-                  <q-avatar color="teal" text-color="white" icon="bluetooth" />
-                </q-item-section>
-              </q-item>
-              <q-separator />
+            <q-item clickable v-ripple>
+              <q-item-section>Avatar-type icon</q-item-section>
+              <q-item-section avatar>
+                <q-avatar color="teal" text-color="white" icon="bluetooth" />
+              </q-item-section>
+            </q-item>
+            <q-separator />
 
-              <q-item clickable v-ripple>
-                <q-item-section>Rounded avatar-type icon</q-item-section>
-                <q-item-section avatar>
-                  <q-avatar rounded color="purple" text-color="white" icon="bluetooth" />
-                </q-item-section>
-              </q-item>
-              <q-separator />
+            <q-item clickable v-ripple>
+              <q-item-section>Rounded avatar-type icon</q-item-section>
+              <q-item-section avatar>
+                <q-avatar rounded color="purple" text-color="white" icon="bluetooth" />
+              </q-item-section>
+            </q-item>
+            <q-separator />
 
-              <q-item clickable v-ripple>
-                <q-item-section>Letter avatar-type</q-item-section>
-                <q-item-section avatar>
-                  <q-avatar color="primary" text-color="white">R</q-avatar>
-                </q-item-section>
-              </q-item>
-            </q-list>
+            <q-item clickable v-ripple>
+              <q-item-section>Letter avatar-type</q-item-section>
+              <q-item-section avatar>
+                <q-avatar color="primary" text-color="white">R</q-avatar>
+              </q-item-section>
+            </q-item>
+          </q-list>
+        </div>
+
+        <q-card-actions align="around">
+          <div class="q-pa-md q-gutter-md"></div>
+        </q-card-actions>
+      </q-card>
+      <q-card class="my-card m-margin-top">
+        <q-card-section class="bg-indigo-3 text-white">
+          <div class="text-h6">标签</div>
+        </q-card-section>
+
+        <q-card-actions align="around">
+          <div class="q-pa-md q-gutter-md">
+            <q-chip square color="primary" text-color="white" icon="event">Add to calendar</q-chip>
+            <q-chip class="glossy" square color="teal" text-color="white" icon="bookmark">Bookmark</q-chip>
+            <q-chip square color="orange" text-color="white" icon-right="star">Star</q-chip>
+            <q-chip square color="red" text-color="white" icon="alarm" label="Set alarm" />
+            <q-chip square color="deep-orange" text-color="white" icon="directions">Get directions</q-chip>
+            <q-chip square color="primary" text-color="white" icon="event">Add to calendar</q-chip>
+            <q-chip class="glossy" square color="teal" text-color="white" icon="bookmark">Bookmark</q-chip>
+            <q-chip square color="orange" text-color="white" icon-right="star">Star</q-chip>
+            <q-chip square color="red" text-color="white" icon="alarm" label="Set alarm" />
+            <q-chip square color="deep-orange" text-color="white" icon="directions">Get directions</q-chip>
           </div>
+        </q-card-actions>
+      </q-card>
+      <q-card class="my-card m-margin-top">
+        <q-card-section class="bg-purple-3 text-white">
+          <div class="text-h6">最新推荐</div>
+        </q-card-section>
 
-          <q-card-actions align="around">
-            <div class="q-pa-md q-gutter-md"></div>
-          </q-card-actions>
-        </q-card>
-      </div>
-      <div>
-        <q-card class="my-card m-margin-top">
-          <q-card-section class="bg-indigo-3 text-white">
-            <div class="text-h6">标签</div>
-          </q-card-section>
+        <q-card-actions align="around">
+          <q-item-label header>List Header</q-item-label>
 
-          <q-card-actions align="around">
-            <div class="q-pa-md q-gutter-md">
-              <q-chip square color="primary" text-color="white" icon="event">Add to calendar</q-chip>
-              <q-chip class="glossy" square color="teal" text-color="white" icon="bookmark">Bookmark</q-chip>
-              <q-chip square color="orange" text-color="white" icon-right="star">Star</q-chip>
-              <q-chip square color="red" text-color="white" icon="alarm" label="Set alarm" />
-              <q-chip square color="deep-orange" text-color="white" icon="directions">Get directions</q-chip>
-              <q-chip square color="primary" text-color="white" icon="event">Add to calendar</q-chip>
-              <q-chip class="glossy" square color="teal" text-color="white" icon="bookmark">Bookmark</q-chip>
-              <q-chip square color="orange" text-color="white" icon-right="star">Star</q-chip>
-              <q-chip square color="red" text-color="white" icon="alarm" label="Set alarm" />
-              <q-chip square color="deep-orange" text-color="white" icon="directions">Get directions</q-chip>
-            </div>
-          </q-card-actions>
-        </q-card>
-      </div>
-      <div>
-        <q-card class="my-card m-margin-top">
-          <q-card-section class="bg-purple-3 text-white">
-            <div class="text-h6">最新推荐</div>
-          </q-card-section>
+          <q-item>
+            <q-item-section avatar></q-item-section>
+            <q-item-section>List item</q-item-section>
+            <q-item-section side>
+              <q-item-label caption>meta</q-item-label>
+            </q-item-section>
+          </q-item>
 
-          <q-card-actions align="around">
-            <q-item-label header>List Header</q-item-label>
+          <q-separator spaced inset="item" />
 
-            <q-item>
-              <q-item-section avatar></q-item-section>
-              <q-item-section>List item</q-item-section>
-              <q-item-section side>
-                <q-item-label caption>meta</q-item-label>
-              </q-item-section>
-            </q-item>
+          <q-item>
+            <q-item-section top avatar>
+              <q-avatar color="primary" text-color="white" icon="bluetooth" />
+            </q-item-section>
 
-            <q-separator spaced inset="item" />
+            <q-item-section>
+              <q-item-label>Single line item</q-item-label>
+              <q-item-label caption lines="2">Secondary line text. Lorem ipsum dolor sit amet, consectetur adipiscit elit.</q-item-label>
+            </q-item-section>
 
-            <q-item>
-              <q-item-section top avatar>
-                <q-avatar color="primary" text-color="white" icon="bluetooth" />
-              </q-item-section>
+            <q-item-section side top>
+              <q-item-label caption>5 min ago</q-item-label>
+              <q-icon name="star" color="yellow" />
+            </q-item-section>
+          </q-item>
 
-              <q-item-section>
-                <q-item-label>Single line item</q-item-label>
-                <q-item-label
-                  caption
-                  lines="2"
-                >Secondary line text. Lorem ipsum dolor sit amet, consectetur adipiscit elit.</q-item-label>
-              </q-item-section>
+          <q-item>
+            <q-item-section top avatar>
+              <q-avatar rounded>
+                <img src="https://cdn.quasar.dev/img/boy-avatar.png" />
+              </q-avatar>
+            </q-item-section>
 
-              <q-item-section side top>
-                <q-item-label caption>5 min ago</q-item-label>
-                <q-icon name="star" color="yellow" />
-              </q-item-section>
-            </q-item>
+            <q-item-section>
+              <q-item-label>Single line item</q-item-label>
+              <q-item-label caption>Secondary line text. Lorem ipsum dolor sit amet, consectetur adipiscit elit.</q-item-label>
+            </q-item-section>
 
-            <q-item>
-              <q-item-section top avatar>
-                <q-avatar rounded>
-                  <img src="https://cdn.quasar.dev/img/boy-avatar.png" />
-                </q-avatar>
-              </q-item-section>
+            <q-item-section side top>
+              <q-item-label caption>meta</q-item-label>
+            </q-item-section>
+          </q-item>
+          <q-separator spaced inset="item" />
 
-              <q-item-section>
-                <q-item-label>Single line item</q-item-label>
-                <q-item-label
-                  caption
-                >Secondary line text. Lorem ipsum dolor sit amet, consectetur adipiscit elit.</q-item-label>
-              </q-item-section>
+          <q-item>
+            <q-item-section top avatar>
+              <q-avatar color="primary" text-color="white" square icon="bluetooth" />
+            </q-item-section>
 
-              <q-item-section side top>
-                <q-item-label caption>meta</q-item-label>
-              </q-item-section>
-            </q-item>
-            <q-separator spaced inset="item" />
+            <q-item-section>
+              <q-item-label>Single line item</q-item-label>
+              <q-item-label caption>Secondary line text. Lorem ipsum dolor sit amet, consectetur adipiscit elit.</q-item-label>
+            </q-item-section>
 
-            <q-item>
-              <q-item-section top avatar>
-                <q-avatar color="primary" text-color="white" square icon="bluetooth" />
-              </q-item-section>
+            <q-item-section side top>
+              <q-item-label caption>meta</q-item-label>
+            </q-item-section>
+          </q-item>
+        </q-card-actions>
+      </q-card>
+      <q-card class="my-card m-margin-top">
+        <q-card-section class="bg-teal text-white">
+          <div class="text-h6">联系我</div>
+        </q-card-section>
 
-              <q-item-section>
-                <q-item-label>Single line item</q-item-label>
-                <q-item-label
-                  caption
-                >Secondary line text. Lorem ipsum dolor sit amet, consectetur adipiscit elit.</q-item-label>
-              </q-item-section>
-
-              <q-item-section side top>
-                <q-item-label caption>meta</q-item-label>
-              </q-item-section>
-            </q-item>
-          </q-card-actions>
-        </q-card>
-      </div>
+        <q-card-actions align="around">
+          <q-img src="https://cdn.quasar.dev/img/mountains.jpg" style="margin-bottom: 0.3em" />
+          <q-btn type="a" href="https://github.com/ligecarryme" glossy color="primary" round flat icon="fab fa-github"></q-btn>
+          <q-btn glossy color="primary" round flat icon="fab fa-qq">
+            <q-tooltip transition-show="rotate" transition-hide="rotate" max-height="200px" max-width="200px">qq</q-tooltip>
+          </q-btn>
+          <q-btn glossy color="primary" round flat icon="fab fa-weixin"></q-btn>
+          <q-btn glossy color="primary" round flat icon="mail"></q-btn>
+          <q-btn glossy color="primary" round flat icon="send"></q-btn>
+        </q-card-actions>
+      </q-card>
     </div>
-
-    <div></div>
   </div>
 </template>
 
@@ -189,23 +193,39 @@ export default {
 };
 </script>
 
-<style lang="sass" scoped>
+<style lang="scss" scoped>
 @import "../assets/css/me.css";
 
-.example-item
-  height: 450px
-  width: 450px
+.card-item {
+  height: 450px;
+  width: 450px;
+}
 
-.my-card
-  width: 100%
-  max-width: 450px
+.my-card {
+  width: 100%;
+  max-width: 430px;
+}
 
-.row > div
-  padding: 5px 10px
-  // background: rgba(86,61,124,.15)
-  // border: 1px solid rgba(86,61,124,.2)
+.imgItem {
+  padding: 6px 6px;
+  .q-img {
+    max-width: 422px;
+  }
+}
+.blogTitle {
+  a {
+    color: #90caf9;
+  }
+  a:hover {
+    color: #2196f3;
+  }
+}
 
-.row + .row
-  margin-top: 1rem
+// .row > div
+//   padding: 5px 10px
+// background: rgba(86,61,124,.15)
+// border: 1px solid rgba(86,61,124,.2)
 
+// .row + .row
+//   margin-top: 1rem
 </style>
