730a1c133952775e98d6a0d521f46cf1c05c7044
ligecarryme <965108861@qq.com>
2021-05-01 13:05:37 +0800

    fix: article页面依赖引入

--- src/quasar.js
+++ src/quasar.js
@@ -3,11 +3,11 @@ import Vue from 'vue'
 import './styles/quasar.sass'
 import '@quasar/extras/material-icons/material-icons.css'
 import '@quasar/extras/fontawesome-v5/fontawesome-v5.css'
-import { Quasar, Notify, Ripple, QLayout, QHeader, QToolbar, QAvatar, QTabs, QToolbarTitle, QRouteTab, QForm, QInput, QDrawer, QBtn, QAjaxBar, QPageContainer, QSeparator, QPagination, QCard, QIcon, QImg, QCardSection, QCardActions, QTooltip, QDialog, QIntersection, QItem, QItemSection, QItemLabel, QBadge, QChip, QList, QTimeline, QTimelineEntry } from 'quasar'
+import { Quasar, Notify, Ripple, QLayout, QHeader, QToolbar, QAvatar, QTabs, QToolbarTitle, QRouteTab, QForm, QInput, QDrawer, QBtn, QAjaxBar, QPageContainer, QSeparator, QPagination, QCard, QIcon, QImg, QCardSection, QCardActions, QTooltip, QDialog, QIntersection, QItem, QItemSection, QItemLabel, QBadge, QChip, QList, QTimeline, QTimelineEntry, QResizeObserver, QPageScroller, QRating, QParallax, QSpace, QMenu, QScrollArea, QScrollObserver, QChatMessage, QEditor, QSelect } from 'quasar'
 
 Vue.use(Quasar, {
   config: {},
-  components: { QLayout, QHeader, QToolbar, QAvatar, QTabs, QToolbarTitle, QRouteTab, QForm, QInput, QDrawer, QBtn, QAjaxBar, QPageContainer, QSeparator, QPagination, QCard, QIcon, QImg, QCardSection, QCardActions, QTooltip, QDialog, QIntersection, QItem, QItemSection, QItemLabel, QBadge, QChip, QList, QTimeline, QTimelineEntry },
+  components: { QLayout, QHeader, QToolbar, QAvatar, QTabs, QToolbarTitle, QRouteTab, QForm, QInput, QDrawer, QBtn, QAjaxBar, QPageContainer, QSeparator, QPagination, QCard, QIcon, QImg, QCardSection, QCardActions, QTooltip, QDialog, QIntersection, QItem, QItemSection, QItemLabel, QBadge, QChip, QList, QTimeline, QTimelineEntry, QResizeObserver, QPageScroller, QRating, QParallax, QSpace, QMenu, QScrollArea, QScrollObserver, QChatMessage, QEditor, QSelect },
   directives: { Ripple },
   plugins: { Notify }
 })
